ce78c669d2be89ef799f101d093928488d61169a
Tyler Beard <tyler.beard47@gmail.com>
2017-06-27 12:00:34 -0400

    Remove conflict error stuff

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,11 +2,6 @@
 public class Hello {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Hello");
-		System.out.println("Hello from the zach branch.");
-		System.out.println("Tyler");
-=======
 		System.out.println("Hello");		
 		System.out.println("Hello from the zach branch.");
 		System.out.println("Tyler");
@@ -14,8 +9,6 @@ public class Hello {
 		//round 2
 		
 		System.out.println("Hello from the zach branch again.");
-
->>>>>>> Zach
 	}
 
 }
