bc21db7d6430672487f6a43db56d6a0a0782bd66
Hornet29 <wassinkz@mail.gvsu.edu>
2017-06-27 11:39:13 -0400

    testing from zach
    comitting to zach

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,13 +2,13 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-<<<<<<< HEAD
-		
+		System.out.println("Hello");		
 		System.out.println("Hello from the zach branch.");
-=======
 		System.out.println("Tyler");
->>>>>>> master
+
+		//round 2
+		
+		System.out.println("Hello from the zach branch again.");
 
 	}
 
